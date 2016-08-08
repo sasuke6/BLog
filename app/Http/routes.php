461092822cbbd85a9583385cf2c11aabc4979aca
Hsu <xusasuke6@gmail.com>
2016-08-09 07:29:26 +0800

    upload the blog project

---
+++ app/Http/routes.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+
+
+Route::group(['middleware' =>  ['web']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/test', 'IndexController@index');
+
+    Route::get('admin/login', 'Admin/LoginController@login');
+});
+
